5893769d32d4e23bc4fbb581e2974498760b4d24
Urip Yogantara <uripyogantara@gmail.com>
2018-10-10 22:32:41 +0800

    register store

--- app/Http/Controllers/AuthUserController.php
+++ app/Http/Controllers/AuthUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\User;
+use App\Store;
 class AuthUserController extends Controller
 {
     protected $successStatus=200;
@@ -27,10 +28,25 @@ class AuthUserController extends Controller
     public function register(Request $request){
         $input = $request->all(); 
         $input['password'] = bcrypt($input['password']); 
-        $user = User::create($input); 
+        $user = User::create([
+            'name'      =>  $input['name'],
+            'username'  =>  $input['username'],
+            'email'     =>  $input['email'],
+            'phone'     =>  $input['phone'],
+            'password'  =>  $input['password'],
+            'user_type' =>  $input['user_type']
+        ]); 
+        if($input['user_type']==2){
+            $store=Store::create([
+                'user_id'=>$user['id'],
+                'name'   => $input['store_name'],
+                'address'=> $input['store_address'],
+                'lat'    =>  $input['lat'],
+                'lng'    =>  $input['lng']
+            ]);
+        }
         $token=  $user->createToken('MyApp'); 
         $user->token=$token->accessToken;
-
         return response()->json($user, $this-> successStatus);
     }
 }
--- app/Store.php
+++ app/Store.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'address', 'lat','lng','user_id'
+    ];
 }
